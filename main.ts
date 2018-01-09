d561771c397fff90214bbbb932810c8bf6dc92bd
Shawn Nock <nock@nocko.se>
2018-01-09 11:22:17 -0500

    Removes function calls due to overhead

--- main.ts
+++ main.ts
@@ -8,77 +8,47 @@ namespace dht11 {
         pins.setPull(pin, PinPullMode.PullUp);
 
     }
-    
-    function wait_for_level(level: number): void {
-        while (pins.digitalReadPin(pin) != level);
-    }
-    
-    function wait_for_low() {
-        wait_for_level(0)
-    }
-    
-    function wait_for_high() {
-        wait_for_level(1)
-    }
 
-    function pin_is_high(): Boolean {
-        return pins.digitalReadPin(pin) == 1;
-    }
-        
     /**
      * Set pin at which the DHT data line is connected
      * @param pin_arg pin at which the DHT data line is connected
      */
-    //% block = "DHT11 is on pin %pin_arg=DigitalPin"
+    //% block = "DHT11 is on pin %pin_arg"
     export function set_pin(pin_arg: DigitalPin): void {
         pin = pin_arg;
     }
-    
+
     //% block
-    export function temperature(): number {
+    function dht11_read(): number {
         signal_dht11(pin);
-        wait_for_low();
-        wait_for_high();
-        wait_for_low();
-        
+
+        // Wait for response header to finish
+        while (pins.digitalReadPin(pin) == 1);
+        while (pins.digitalReadPin(pin) == 0);
+        while (pins.digitalReadPin(pin) == 1);
+
         let value = 0;
         let counter = 0;
-            
+
         for (let i = 0; i <= 32 - 1; i++) {
-            wait_for_high();
+            while (pins.digitalReadPin(pin) == 0);
             counter = 0
-            while (pin_is_high()) {
+            while (pins.digitalReadPin(pin) == 1) {
                 counter += 1;
             }
             if (counter > 4) {
                 value = value + (1 << (31 - i));
             }
         }
-        let temperature: number = (value & 0x0000ff00) >> 8;
-        return temperature;
+        return value;
+    }
+
+    export function temperature(): number {
+        return (dht11_read() & 0x0000ff00) >> 8;
     }
 
     //% block
     export function humidity(): number {
-        signal_dht11(pin);
-        wait_for_low();
-        wait_for_high();
-        wait_for_low();
-        
-        let value = 0;
-        let counter = 0;
-            
-        for (let i = 0; i <= 32 - 1; i++) {
-            wait_for_high();
-            counter = 0
-            while (pin_is_high()) {
-                counter += 1;
-            }
-            if (counter > 4) {
-                value = value + (1 << (31 - i));
-            }
-        }
-        let humidity: number = (value >> 24);
-        return humidity;
+        return dht11_read() >> 24
     }
 }
--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
+serial.writeString("Alive\n\r");
 dht11.set_pin(DigitalPin.P0)
 basic.forever(() => {
     serial.writeValue("Temperature", dht11.temperature())
