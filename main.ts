89bd6e0b024a666dec49c4586f86e67719c9784d
Shawn Nock <nock@nocko.se>
2018-01-09 12:02:50 -0500

    Block formatting

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ namespace dht11 {
      * @param pin_arg pin at which the DHT data line is connected
      */
     //% blockID=dht11_set_pin
-    //% block="Setup DHT11 on pin %pin=DigitalPin"
+    //% block="Setup DHT11 on pin %pin_arg=DigitalPin"
     export function set_pin(pin_arg: DigitalPin): void {
         pin = pin_arg;
     }
