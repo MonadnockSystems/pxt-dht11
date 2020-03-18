ba3762380c24db53cd4885c0391b5eef38bc6002
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-03-18 21:48:37 +0100

    Get both values during one read from sensor. Returns static Temperature & Humidity

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 //% color=#f44242 icon="\uf197" block="Let's Talk Science"
 namespace dht11 {
     let pin = DigitalPin.P0;
+    let DTH11value = 0;
     function signal_dht11(pin: DigitalPin): void {
         pins.digitalWritePin(pin, 0)
         basic.pause(18)
@@ -43,13 +44,31 @@ namespace dht11 {
         return value;
     }
 
-    //% block
-    export function temperature(): number {
-        return (dht11_read() & 0x0000ff00) >> 8;
+    /**
+     * Executes reading from sensor - both Temperature & Humidity
+     * to retrieve static values -> get temperature() or humidity() 
+     */
+    //% block="DHT11 ReadFromSensor"
+    //% blockId=dht11_read_from_sensor
+    export function ReadFromSensor() {
+        DTH11value=dht11_read();
     }
 
+    /**
+     * Returns previously read temperature()
+     * Do not forget to call ReadFromSensor() to get current ones 
+     */
+    //% block
+    export function temperature(): number {
+        return (DTH11value & 0x0000ff00) >> 8;
+    }
+
+    /**
+     * Returns previously read humidity()
+     * Do not forget to call ReadFromSensor() to get current ones 
+     */
     //% block
     export function humidity(): number {
-        return dht11_read() >> 24
+        return DTH11value >> 24
     }
 }
